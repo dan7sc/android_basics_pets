23f249cb8d69a0a5a4888cd37ba5a284740925a5
dan7sc <su7cheng@gmail.com>
2019-05-27 12:36:32 -0300

    Add constants for pet content URIs.

--- app/src/main/java/com/example/android/pets/data/PetContract.kt
+++ app/src/main/java/com/example/android/pets/data/PetContract.kt
@@ -16,12 +16,34 @@
 
 package com.example.android.pets.data
 
+import android.net.Uri
 import android.provider.BaseColumns
 
 /**
  * API Contract for the Pets app.
  */
 object PetContract {
+    /**
+     * The "Content authority" is a name for the entire content provider, similar to the
+     * relationship between a domain name and its website.  A convenient string to use for the
+     * content authority is the package name for the app, which is guaranteed to be unique on the
+     * device.
+     */
+    val CONTENT_AUTHORITY = "com.example.android.pets"
+
+    /**
+     * Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+     * the content provider.
+     */
+    val BASE_CONTENT_URI = Uri.parse("content://$CONTENT_AUTHORITY")
+
+    /**
+     * Possible path (appended to base content URI for possible URI's)
+     * For instance, content://com.example.android.pets/pets/ is a valid path for
+     * looking at pet data. content://com.example.android.pets/staff/ will fail,
+     * as the ContentProvider hasn't been given any information on what to do with "staff".
+     */
+    val PATH_PETS = "pets"
 
     /**
      * Inner class that defines constant values for the pets database table.
@@ -29,6 +51,9 @@ object PetContract {
      */
     class PetEntry : BaseColumns {
         companion object {
+
+            /** The content URI to access the pet data in the provider  */
+            val CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS)
 
             /** Name of database table for pets  */
             const val TABLE_NAME: String = "pets"
